3e06d00be225e541ab1dd0d4a636c9261f15f94e
jessalbarian <albarian.jess@gmail.com>
2016-07-14 21:59:27 -0600

    changed name of ios project

---
+++ iOSApp/CUApp/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CUApp
+//
+//  Created by Jessie Albarian on 6/6/16.
+//  Copyright © 2016 babyllama. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
---
+++ iOSApp/CUApp/Desire2Learn.swift
@@ -0,0 +1,14 @@
+//
+//  Desire2Learn.swift
+//  CUApp
+//
+//  Created by Jessie Albarian on 7/13/16.
+//  Copyright © 2016 babyllama. All rights reserved.
+//
+
+import UIKit
+
+class Desire2Learn: UIWebView {
+
+
+}
