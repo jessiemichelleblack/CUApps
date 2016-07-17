d813078932038d3b500d08fd96b76a313c1270d4
jessalbarian <albarian.jess@gmail.com>
2016-07-16 22:10:04 -0600

    added new version of ios app

---
+++ iOSCampusApp/iOSCampusApp/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  iOSCampusApp
+//
+//  Created by Jessie Albarian on 7/16/16.
+//  Copyright © 2016 LittleBirdStudios. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var iconText: UILabel!
+    @IBOutlet var iconImage: UIImageView!
+}
