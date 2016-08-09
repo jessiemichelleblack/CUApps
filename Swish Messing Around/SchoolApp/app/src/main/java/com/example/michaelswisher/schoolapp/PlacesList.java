4621b48b9282dbc6234bea8a697f4dfcb417a0b6
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2016-08-08 21:18:45 -0600

    Fixed the display issue with rendering firebase data into a listview. Going to start prototyping the map view.

--- Swish Messing Around/SchoolApp/app/src/main/java/com/example/michaelswisher/schoolapp/PlacesList.java
+++ Swish Messing Around/SchoolApp/app/src/main/java/com/example/michaelswisher/schoolapp/PlacesList.java
@@ -1,14 +1,11 @@
 package com.example.michaelswisher.schoolapp;
 
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.firebase.client.Firebase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,12 +19,7 @@ public class PlacesList extends AppCompatActivity {
 
     ListView listview;
     SearchView searchview;
-
-    //private DatabaseReference mDatabase;
-
-    String[] places = {""};
     public List<String> placesList = new ArrayList<String>();
-
     ArrayAdapter<String> adapter;
 
     @Override
@@ -35,30 +27,30 @@ public class PlacesList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_list);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("places");
-        System.out.println(myRef);
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
-                    String placeName = messageSnapshot.getKey();
-                    System.out.println(placeName + " : " + messageSnapshot.getValue());
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                //Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-
         listview= (ListView) findViewById(R.id.placesList);
         searchview = (SearchView) findViewById(R.id.placesSearch);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, places);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, placesList);
         listview.setAdapter(adapter);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("places");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+                            String placeName = messageSnapshot.getKey();
+                            placesList.add(placeName);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                    }
+        });
+
 
         searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -68,19 +60,9 @@ public class PlacesList extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                System.out.println("Array size: " + places.length);
-                System.out.println("List size: " + placesList.size());
                 adapter.getFilter().filter(newText);
-
                 return false;
             }
         });
     }
-
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        setContentView(R.layout.activity_places_list);
-//        System.out.println("wtf " + placesList.size());
-//    }
 }
