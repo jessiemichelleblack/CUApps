7aac62067f358e94e9fc143637cbe646c1625c72
jessalbarian <albarian.jess@gmail.com>
2016-08-10 22:07:30 -0600

    Changed the Place object data structure to contain an attribute for the picture names.

--- iOSSchoolApp/iOSSchoolApp/Place.swift
+++ iOSSchoolApp/iOSSchoolApp/Place.swift
@@ -13,18 +13,32 @@ class Place {
     var latCoordinate : String
     var longCoordinate : String
     var placeType : String
+    var pictureName : String
     
+    //Default constructor
     init (){
         self.name = ""
         self.latCoordinate = ""
         self.longCoordinate = ""
         self.placeType = ""
+        self.pictureName = ""
     }
     
+    //Constructor for items without a picture
     init (newname : String, newlat : String, newlong : String, newtype : String){
         self.name = newname
         self.latCoordinate = newlat
         self.longCoordinate = newlong
         self.placeType = newtype
+        self.pictureName = ""
+    }
+    
+    //Constructor for items that do have a picture
+    init (newname : String, newlat : String, newlong : String, newtype : String, newPictureName : String){
+        self.name = newname
+        self.latCoordinate = newlat
+        self.longCoordinate = newlong
+        self.placeType = newtype
+        self.pictureName = newPictureName
     }
 }
