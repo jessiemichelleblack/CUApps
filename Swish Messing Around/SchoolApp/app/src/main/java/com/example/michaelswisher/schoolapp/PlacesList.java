56c7f38b82318c47e30bdcbabf0b6c219581d484
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2016-08-14 23:34:32 -0600

    Reintegrated firebase to app over couchdb on personal vps.

--- Swish Messing Around/SchoolApp/app/src/main/java/com/example/michaelswisher/schoolapp/PlacesList.java
+++ Swish Messing Around/SchoolApp/app/src/main/java/com/example/michaelswisher/schoolapp/PlacesList.java
@@ -1,9 +1,7 @@
 package com.example.michaelswisher.schoolapp;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -11,29 +9,17 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.google.common.base.Charsets;
-import com.google.common.io.ByteStreams;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class PlacesList extends AppCompatActivity {
 
@@ -41,6 +27,8 @@ public class PlacesList extends AppCompatActivity {
     SearchView searchview;
     List<String> placesList = new ArrayList<String>();
     ArrayAdapter<String> adapter;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference myRef = database.getReference("places");
 
     HashMap placesContainer = new HashMap();
 
@@ -55,41 +43,29 @@ public class PlacesList extends AppCompatActivity {
         searchview = (SearchView) findViewById(R.id.placesSearch);
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, placesList);
         listview.setAdapter(adapter);
-
-        new URLDataDownloadPlaces().execute("http://swishertest.site/");
-//        System.out.println(result);
-    }
-
-    public class URLDataDownloadPlaces extends URLDataDownload {
-
-        public URLDataDownloadPlaces() {
-            progressDialog = new ProgressDialog(PlacesList.this);
-        }
-
-        /**
-         * onPostExecute shows website data
-         */
-        protected void onPostExecute(Void v) {
-            progressDialog.dismiss();
-            /*
-            System.out.println(result);
-            try {
-
-                JSONObject resultObject = new JSONObject(result);
-                JSONArray placesArray = resultObject.getJSONArray("places");
-                for (int i = 0; i < placesArray.length(); i++) {
-                    JSONObject place = placesArray.getJSONObject(i);
-                    System.out.println(place);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            */
-        }
     }
 
     @Override protected void onResume(){
         super.onResume();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+                    String placeName = messageSnapshot.getKey();
+                    placesList.add(placeName);
+                    ArrayList<String> placeValues = (ArrayList<String>) messageSnapshot.getValue();
+                    placesContainer.put(placeName, placeValues);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+            }
+        });
+
+
         searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -110,7 +86,6 @@ public class PlacesList extends AppCompatActivity {
             {
                 String value = (String)adapter.getItemAtPosition(position);
                 ArrayList<String> valueSet = (ArrayList<String>) placesContainer.get(value);
-
                 /* Using the local maps activity */
 //                Intent intent = new Intent(PlacesList.this, MapsActivity.class);
 //                intent.putExtra("LocationName", value);
@@ -119,7 +94,6 @@ public class PlacesList extends AppCompatActivity {
 //                startActivity(intent);
 
                 /* Using google maps app externally */
-
                 String mapsQuery = String.format("geo:0,0?q=%s,%s (%s)", valueSet.get(0), valueSet.get(1), value);
                 Uri gmmIntentUri = Uri.parse(mapsQuery);
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
@@ -128,5 +102,4 @@ public class PlacesList extends AppCompatActivity {
             }
         });
     }
-
 }
